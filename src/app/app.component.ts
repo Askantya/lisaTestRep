f3a2e6be755b6b7812e914b1e5cca5af78d86b41
Daniil Mikhailov <daniil.mikhailov@innowise-group.com>
2021-07-22 22:13:23 +0300

    add expamples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LisaAngular';
+export class AppComponent implements OnInit {
+
+  public variable1 = '';
+  private variable2 = 123;
+
+  public ngOnInit(): void {
+    const a = 123;
+    const b = 3;
+    console.log(a + b);
+  }
+
+  public onCloseClick(): void {
+    this.variable1 += '1';
+  }
 }
