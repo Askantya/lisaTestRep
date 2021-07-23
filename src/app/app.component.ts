1013ecfbb451686bcc8edffd1466fdbf50603216
Liza <lizuha1860@gmail.com>
2021-07-23 18:39:07 +0300

    optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent implements OnInit {
 
   public variable1 = '';
   private variable2 = 123;
-  public variable3 = '';
+  public selectedAnimal = '';
 
   public ngOnInit(): void {
     const a = 123;
@@ -21,16 +21,7 @@ export class AppComponent implements OnInit {
     this.variable1 += '1';
   }
 
-  public onCatClick(): void{
-    // название переменной всегда должно отображать то что оно делает, например не variable3
-    // a selectedAnimal и сразу становится намного понятнее
-    this.variable3 = 'Выбранное животное:{{Кот}}';
-    // фигурные скобки были как бы намек на то чтобы в html это вставить)))
-  }
-  public onDogClick(): void{
-    this.variable3 = 'Выбранное животное:{{Собака}}';
-  }
-  public onKapibaraClick(): void{
-    this.variable3 = 'Выбранное животное:{{Капибара}}';
+  public onAnimalClick(selectedAnimal: string): void{
+    this.selectedAnimal = selectedAnimal;
   }
 }
