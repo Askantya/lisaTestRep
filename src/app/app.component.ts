0c5fe4d749dc074894d26eda9cd8c299ee83cba5
Liza <lizuha1860@gmail.com>
2021-07-25 23:39:49 +0300

    star string number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   public ngOnInit(): void {
-    
+    this.starStrings(3);
   }
 
+  private starStrings(starNum: number): void {
+    let star = '';
+    for(let i=1; i<=starNum; i++){
+      for(let p=1; p<=i; p++){
+        star += '*';
+      }
+      console.log(star);
+      star = '';
+    }
+  }
 }
