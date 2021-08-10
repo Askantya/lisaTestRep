d8020952f119edb2a1e2021d481c139a029b6198
Liza <lizuha1860@gmail.com>
2021-08-10 23:07:29 +0300

    fill array matrix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-for-of */
+import { keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   public ngOnInit(): void {
-
+    const newMatrixArray = this.matrixArray(4, 5);
   }
 
-
+  private matrixArray(firstNumber: number, secondNumber: number): void {
+    const array1 = new Array(secondNumber).fill(0).map( () => new Array(firstNumber).fill(0));
+    let k = 1;
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array1[i].length; j++) {
+        array1[i][j] = k;
+        k++;
+      }
+    }
+    console.log(array1);
+  };
 }
