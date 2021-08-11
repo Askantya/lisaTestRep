49125ea0865f9a78153092dc23e314af110a2ad4
Liza <lizuha1860@gmail.com>
2021-08-11 22:06:22 +0300

    shift element of array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   public ngOnInit(): void {
     const newMatrixArray = this.matrixArray(4, 5);
+    this.shiftingArrayElements(4);
   }
 
   private matrixArray(firstNumber: number, secondNumber: number): void {
@@ -23,4 +24,23 @@ export class AppComponent implements OnInit {
     }
     console.log(array1);
   };
+
+  private shiftingArrayElements(countOfElements: number): void {
+    const arrayElements = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+    const arrayOneElements = [];
+    for (let i = 0; i < arrayElements.length; i++){
+      for (let j = 0; j < arrayElements[i].length; j++){
+        arrayOneElements.push(arrayElements[i][j]);
+      }
+    }
+    const indexOfElements = 9 - countOfElements;
+    const sliceArray = arrayOneElements.slice(indexOfElements, 9);
+    arrayOneElements.splice(indexOfElements, countOfElements);
+    const concatArray = sliceArray.concat(arrayOneElements);
+    const array1 = concatArray.slice(0, 3);
+    const array2 = concatArray.slice(3, 6);
+    const array3 = concatArray.slice(6, 9);
+    const arrayUnification = new Array(array1, array2, array3);
+    console.log(arrayUnification);
+  }
 }
