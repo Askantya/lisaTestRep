700c249453bf12aab11222717bd86cdb3d997705
Daniil Mikhailov <daniil.mikhailov@innowise-group.com>
2021-08-20 22:53:31 +0300

    ебанарот

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+// УБРАТЬ, ИСПОЛЬЗОВАТЬ ВМЕСТО НЕГО ИНТЕРФЕЙС ЕБАНАРОТ
 class StringTable {
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   name: string;
   // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   description: string;
 
-  constructor(name: string, description: string){
+  constructor(name: string, description: string) {
     this.name = name;
     this.description = description;
   }
@@ -17,27 +18,35 @@ class StringTable {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent implements OnInit {
-
+  // НЕЙМИНГ ЕБАНАРОТ
   public firstCell = '';
   public secondCell = '';
 
+  // ИНТЕРФЕЙС ЕБАНАРОТ
   public arrayOfTable = [
+    // ПУСТОЙ МАССИВ ДОЛЖЕН БЫТЬ ЕБАНАРОТ
+
     {
-      name:'',
-      description: ''
-    }
+      name: '',
+      description: '',
+    },
   ];
 
   public ngOnInit(): void {
-    if(true){}
+    if (true) {
+    }
   }
 
+  // НЕЙМИНГ ЕБАНАРОТ
   public addStringToTable(firstСell: string, secondCell: string): void {
-    if ( (this.arrayOfTable[0].name === '') && (this.arrayOfTable[0].description === '') ) {
+    // УБРАТЬ ИФ ЕБАНАРОТ
+    if (this.arrayOfTable[0].name === '' && this.arrayOfTable[0].description === '') {
       this.arrayOfTable[0].name = firstСell;
       this.arrayOfTable[0].description = secondCell;
+      // ПОЧИСТИ ПОЛЯ ЕБАНАРОТ
+
+      // НЕОБЯЗАТЕЛЬНОЕ ПОЛЯ ЕБАНАРОТ
     } else this.arrayOfTable.push(new StringTable(firstСell, secondCell));
   }
 }
