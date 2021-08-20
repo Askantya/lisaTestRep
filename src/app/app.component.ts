14e303ab43187a724d4cd308b81ef30478b00d15
Liza <lizuha1860@gmail.com>
2021-08-20 21:34:58 +0300

    add string to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+class StringTable {
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  description: string;
+
+  constructor(name: string, description: string){
+    this.name = name;
+    this.description = description;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
 
-  public array = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+  public firstCell = '';
+  public secondCell = '';
 
-  public field = 'asd';
+  public arrayOfTable = [
+    {
+      name:'',
+      description: ''
+    }
+  ];
 
   public ngOnInit(): void {
-    if (true) {
-
-    }
+    if(true){}
   }
 
-  public onClickButton(): void {
-    console.log('Button clicked');
+  public addStringToTable(firstСell: string, secondCell: string): void {
+    if ( (this.arrayOfTable[0].name === '') && (this.arrayOfTable[0].description === '') ) {
+      this.arrayOfTable[0].name = firstСell;
+      this.arrayOfTable[0].description = secondCell;
+    } else this.arrayOfTable.push(new StringTable(firstСell, secondCell));
   }
 }
