5fab698322fb71c794433518141655c1385e9b36
Liza <lizuha1860@gmail.com>
2021-08-30 13:10:45 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-// УБРАТЬ, ИСПОЛЬЗОВАТЬ ВМЕСТО НЕГО ИНТЕРФЕЙС ЕБАНАРОТ
-class StringTable {
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+interface StringTable {
   name: string;
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-  description: string;
-
-  constructor(name: string, description: string) {
-    this.name = name;
-    this.description = description;
-  }
+  description?: string;
 }
 
 @Component({
@@ -19,34 +11,31 @@ class StringTable {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  // НЕЙМИНГ ЕБАНАРОТ
-  public firstCell = '';
-  public secondCell = '';
-
-  // ИНТЕРФЕЙС ЕБАНАРОТ
-  public arrayOfTable = [
-    // ПУСТОЙ МАССИВ ДОЛЖЕН БЫТЬ ЕБАНАРОТ
-
-    {
-      name: '',
-      description: '',
-    },
-  ];
+  public firstValue = '';
+  public secondValue = '';
+  public arrayOfTable: StringTable[] = [];
 
   public ngOnInit(): void {
     if (true) {
     }
   }
 
-  // НЕЙМИНГ ЕБАНАРОТ
-  public addStringToTable(firstСell: string, secondCell: string): void {
-    // УБРАТЬ ИФ ЕБАНАРОТ
-    if (this.arrayOfTable[0].name === '' && this.arrayOfTable[0].description === '') {
-      this.arrayOfTable[0].name = firstСell;
-      this.arrayOfTable[0].description = secondCell;
-      // ПОЧИСТИ ПОЛЯ ЕБАНАРОТ
+  public addStringToTable(firstValue: string, secondValue?: string): void {
+    const newRow: StringTable = {
+      name: firstValue,
+      description: secondValue
+    };
+    this.arrayOfTable.push(newRow);
+  }
 
-      // НЕОБЯЗАТЕЛЬНОЕ ПОЛЯ ЕБАНАРОТ
-    } else this.arrayOfTable.push(new StringTable(firstСell, secondCell));
+  public checkboxClick(valueCheckbox: boolean): any{
+    if(valueCheckbox) {
+      valueCheckbox = false;
+      this.arrayOfTable[1].description = '';
+    }
+    else
+    if(!valueCheckbox) {
+      valueCheckbox = true;
+    }
   }
 }
